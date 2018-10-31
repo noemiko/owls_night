36b1fee824963883dd066288c1aa474b08cc6ced
n.kowalewska <n.kowalewska@clearcode.cc>
2018-10-31 23:44:34 +0100

    basic app with auth

---
+++ app/src/main/java/com/owl/owlsnight/User.java
@@ -0,0 +1,18 @@
+package com.owl.owlsnight;
+
+/**
+ * Created by Belal on 4/15/2018.
+ */
+
+public class User {
+    public String name, email;
+
+    public User(){
+
+    }
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+}
