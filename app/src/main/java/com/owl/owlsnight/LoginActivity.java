f6c3c7dcfc464cacc9bf02cceab0579818a26665
n.kowalewska <n.kowalewska@clearcode.cc>
2018-10-31 23:44:34 +0100

    basic app with auth

---
+++ app/src/main/java/com/owl/owlsnight/LoginActivity.java
@@ -0,0 +1,114 @@
+package com.owl.owlsnight;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText  editTextEmail, editTextPassword;
+    private ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        editTextEmail = findViewById(R.id.edit_text_email_login);
+        editTextPassword = findViewById(R.id.edit_text_password_login);
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.setVisibility(View.GONE);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        findViewById(R.id.button_login).setOnClickListener(this);
+        findViewById(R.id.button_move_to_register).setOnClickListener(this);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (mAuth.getCurrentUser() != null) {
+            //handle the already login user
+        }
+    }
+
+    private void loginUser() {
+        final String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError(getString(R.string.input_error_email));
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError(getString(R.string.input_error_email_invalid));
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError(getString(R.string.input_error_password));
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError(getString(R.string.input_error_password_length));
+            editTextPassword.requestFocus();
+            return;
+        }
+
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()) {
+
+                            Toast.makeText(LoginActivity.this, "Zalogowałeś się", Toast.LENGTH_LONG).show();
+
+                        } else {
+                            Toast.makeText(LoginActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_move_to_register:
+                Intent intent = new Intent(v.getContext(), MainActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.button_login:
+
+                loginUser();
+                break;
+        }
+    }
+}
